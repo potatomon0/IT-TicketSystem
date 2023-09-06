7a5500d44dcb38c355bca6e9169564bb1ebd5360
Hu <huang.h0709@gmail.com>
2023-09-06 17:00:16 -0400

    editing css

--- src/components/TicketDetail/TicketDetail.jsx
+++ src/components/TicketDetail/TicketDetail.jsx
@@ -1,6 +1,6 @@
 import { AppContext } from '../../contexts/context'
-import { useContext } from 'react';
-import {Link} from 'react-router-dom'
+import { useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom'
 import styles from './TicketDetails.module.css'
 // import * as ticketsAPI from '../../utilities/tickets-api'
 
@@ -8,8 +8,7 @@ export default function TicketDetail() {
     const { currentTicket, isSelected } = useContext(AppContext)
     return (
         <div className={`${styles.TicketDetailMain}`}>
-            
-            <div>{currentTicket.email}</div>
+            <div className={`${styles.TicketDetailEmail}`}>{currentTicket.email}</div>
             <div>{currentTicket.name}</div>
             <div>{currentTicket.details}</div>
             {isSelected ? <Link to='/user/edit-Ticket'>Edit</Link> : null}
